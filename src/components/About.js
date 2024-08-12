cc77e43db828c16751bd5f863883bbdadc86c84d
annewaithaka <annewaithaka68@gmail.com>
2024-08-12 14:20:55 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
 
-function About() {
-  return <div>About</div>;
-}
+function About  () {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is a brief paragraph about me.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
+};
 
 export default About;
